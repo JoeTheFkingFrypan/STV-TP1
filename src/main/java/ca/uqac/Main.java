91516a7bb708c9b346bd4cd8b539679b19bf6203
Romain GAGNAIRE <romgagnaire@gmail.com>
2016-02-14 17:58:50 -0500

    - [CHANGE] App now expect file path as argument and will fallback to resource file if none is found (fixes #4)

--- src/main/java/ca/uqac/Main.java
+++ src/main/java/ca/uqac/Main.java
@@ -1,41 +1,37 @@
 package ca.uqac;
 
+import ca.uqac.core.FileLoader;
+import ca.uqac.core.FileLoaderException;
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Document document = null;
+        String fallback = "log.xml";
+
         try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            File inputFile = new File(classLoader.getResource("log.xml").getFile());
-            SAXBuilder saxBuilder = new SAXBuilder();
-            Document document = saxBuilder.build(inputFile);
-
-            List<Element> events = document.getRootElement().getChildren();
-
-            System.out.println("\n======================= [QUESTION #1] =======================\n");
-            Q1.findSessionsLastingForOverOneHour(events);
-            System.out.println("\n======================= [QUESTION #2] =======================\n");
-            Q2.findFilesWrittenWithoutBeingOpened(events);
-            System.out.println("\n======================= [QUESTION #3] =======================\n");
-            Q3.findConcurrentApplications(events);
-            System.out.println("\n======================= [QUESTION #4] =======================\n");
-            Q4.findLineEndings(events);
-            System.out.println("\n======================= [QUESTION #5] =======================\n");
-            Q5.findBruteForceAttempts(events);
-
-        } catch (IOException | JDOMException e) {
-            System.err.println("An error occured while processing file");
-            e.printStackTrace();
-        } catch (NullPointerException e2) {
-            e2.printStackTrace();
+            document = FileLoader.loadExternalFile(args[0]);
+        } catch (FileLoaderException e) {
+            System.err.println("[FALLBACK] Now using resource file '" + fallback + "' as a fallback (located at src/main/resources/log.xml)");
+            document = FileLoader.loadResourceFile(fallback);
         }
+
+        List<Element> events = document.getRootElement().getChildren();
+
+        System.out.println("\n======================= [QUESTION #1] =======================\n");
+        Q1.findSessionsLastingForOverOneHour(events);
+        System.out.println("\n======================= [QUESTION #2] =======================\n");
+        Q2.findFilesWrittenWithoutBeingOpened(events);
+        System.out.println("\n======================= [QUESTION #3] =======================\n");
+        Q3.findConcurrentApplications(events);
+        System.out.println("\n======================= [QUESTION #4] =======================\n");
+        Q4.findLineEndings(events);
+        System.out.println("\n======================= [QUESTION #5] =======================\n");
+        Q5.findBruteForceAttempts(events);
     }
 }
