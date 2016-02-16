b6ced1aeaa11d054d6fee3be0d28b9becfa71239
Romain GAGNAIRE <romgagnaire@gmail.com>
2016-02-15 23:46:51 -0500

    - [ADDITION] App now expect question number as second parameter (optional parameter).
    If this parameter is not present, it will run all question on provided file (or on fallback, if there's anything wrong with the provided file)

--- src/main/java/ca/uqac/Main.java
+++ src/main/java/ca/uqac/Main.java
@@ -12,8 +12,9 @@ public class Main {
     public static void main(String[] args) {
 
         Document document = null;
+        Integer questionNumber = null;
         String fallback = "log.xml";
-        
+
         try {
             switch (args.length) {
                 case 0:
@@ -21,12 +22,19 @@ public class Main {
 
                 case 1:
                     document = FileLoader.loadExternalFile(args[0]);
+                    System.out.println("[INFO] No other parameter detected (no question number provided). Running all questions on loaded file");
+                    break;
+
+                case 2:
+                    document = FileLoader.loadExternalFile(args[0]);
+                    questionNumber = Integer.parseInt(args[1]);
                     break;
 
                 default:
                     System.err.println("[WARN] The application only requires one parameter (path to the log file to be parsed)");
                     System.err.println("[WARN] Trying to read file at " + args[0] + " (all other parameters will be ignored)");
-                    System.err.println("[WARN] Usage : program <logPath>");
+                    System.err.println("[WARN] Usage : 'program <logPath> <questionNumber>'");
+                    System.err.println("[WARN] Question number parameter is optional");
                     document = FileLoader.loadExternalFile(args[0]);
                     break;
             }
@@ -37,16 +45,41 @@ public class Main {
         }
 
         List<Element> events = document.getRootElement().getChildren();
+        if(null != questionNumber) {
+            runQuestion(questionNumber, events);
+        } else {
+            for(int i=1; i<=5; i++) {
+                runQuestion(i, events);
+            }
+        }
+    }
 
-        System.out.println("\n======================= [QUESTION #1] =======================\n");
-        Q1.findSessionsLastingForOverOneHour(events);
-        System.out.println("\n======================= [QUESTION #2] =======================\n");
-        Q2.findFilesWrittenWithoutBeingOpened(events);
-        System.out.println("\n======================= [QUESTION #3] =======================\n");
-        Q3.findConcurrentApplications(events);
-        System.out.println("\n======================= [QUESTION #4] =======================\n");
-        Q4.findLineEndings(events);
-        System.out.println("\n======================= [QUESTION #5] =======================\n");
-        Q5.findBruteForceAttempts(events);
+    private static void runQuestion(int questionNumber, List<Element> events) {
+        switch (questionNumber) {
+            case 1:
+                System.out.println("\n======================= [QUESTION #1] =======================\n");
+                Q1.findSessionsLastingForOverOneHour(events);
+                break;
+
+            case 2:
+                System.out.println("\n======================= [QUESTION #2] =======================\n");
+                Q2.findFilesWrittenWithoutBeingOpened(events);
+                break;
+
+            case 3:
+                System.out.println("\n======================= [QUESTION #3] =======================\n");
+                Q3.findConcurrentApplications(events);
+                break;
+
+            case 4:
+                System.out.println("\n======================= [QUESTION #4] =======================\n");
+                Q4.findLineEndings(events);
+                break;
+
+            case 5:
+                System.out.println("\n======================= [QUESTION #5] =======================\n");
+                Q5.findBruteForceAttempts(events);
+                break;
+        }
     }
 }
